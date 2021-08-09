bca67e114a8e5fabf7dfe4e542fe63c55787aa1c
TokaAbdelhakim <60901301+TokaAbdelhakim@users.noreply.github.com>
2021-08-09 14:47:56 +0200

    Revert "Generation and validation merged to master"

--- Game_module.py
+++ Game_module.py
@@ -3,15 +3,12 @@ import random
 
 class Game:
 
-    winning_count: int
-
-    def __init__(self):
+    def __init__(self, game_count, winning_count, lost_count):
         self.user_input = ""
+        self.game_count = game_count
+        self.winning_count = winning_count
+        self.lost_count = lost_count
         self.max_tries = 10
-        self.game_count = 0
-        self.winning_count = 0
-        self.lost_count = 0
-        self.welcome_and_print()
 
     def generate_random_num(self):
         x = random.randint(0, 100)
@@ -35,8 +32,7 @@ class Game:
                         if try_count < self.max_tries:
                             user_in = self.get_user_input()
                         else:
-                            print(f"the number was {ran_num}")
-                            self.loser_play_again()
+                            print("that was your last try")
                             self.game_count += 1
                             self.lost_count += 1
                             condition = False
@@ -45,13 +41,13 @@ class Game:
                         if try_count < self.max_tries:
                             user_in = self.get_user_input()
                         else:
-                            print(f"the number was {ran_num}")
-                            self.loser_play_again()
+                            print("that was your last try")
                             self.game_count += 1
                             self.lost_count += 1
                             condition = False
                     elif int(user_in) == ran_num:
-                        print(f"that's it, well done, Congratulation ") 
+                        print(f"that's it, well done, Congratulation \n"
+                              f"you made it in {try_count} tries")
                         self.game_count += 1
                         self.winning_count += 1
                         self.winner_play_again(try_count)
@@ -67,54 +63,39 @@ class Game:
                 print("invalid, your input must be number and between 0 and 100")
                 user_in = self.get_user_input()
 
-    def winner_play_again(self, try_count):  
-        if try_count < 10:
+    def winner_play_again(self,try_count):  # toka
+        if try_count<10:
             print("You Still Have Tries,Let's Challenge Again")
             ran_num = self.generate_random_num()
             inp = self.get_user_input()
-            self.validate_and_play(inp, ran_num, try_count)
+            self.validate_and_play(inp, ran_num,try_count)
+            
+    def loser_play_again(self):  # mostafa
+        print("")
 
-    def loser_play_again(self):  
-        print("that was your last try")
-        print('you lost')
-        answer = input('play again?(Y/N): ')
-        if answer.upper() == 'Y':
-            #play again
-            ran_num = self.generate_random_num()
-            inp = self.get_user_input()
-            self.validate_and_play(inp, ran_num)
-        elif answer.upper() == 'N':
-            #close the game
-            return
-        else:
-            print('invalid input')
-
-    def welcome_and_print(self):  
-        game_file = open("game.txt", "r+")  
+    def welcome_and_print():  # Nour
+        game_file = open("E:\game.txt", "r+") #adding file name or path
         (game_file.readable())
-        i = 0
         for x in game_file.readlines():
-            if i == 0:
-                print("Number of played games = " + x)
-                self.game_count = int(x)
-            elif i == 1:
-                print("Number of wins = " + x)
-                self.winning_count = int(x)
-            elif i == 2:
-                print("Number of losses = " + x)
-                self.lost_count = int(x)
-            i += 1
+            print(x)
 
         game_file.close()
+        
 
-    def save_player_data(self):  
+    def save_player_data(self):  # Nour
         g = self.game_count
         w = self.winning_count
-        l = self.lost_count
-        game_file = open("game.txt", "r+")  # adding file name or path
-        game_file.write(f'{str(g)}\n')
-        game_file.write(f'{str(w)}\n')
-        game_file.write(f'{str(l)}\n')
+        l = self.lost_count = 
+        game_file = open("E:\game.txt", "r+") #adding file name or path
+        game_file.write("Number of played games = " + str(g))
+        game_file.write("\nNumber of wins = " + str(w))
+        game_file.write("\nNumber of losses = " + str(l))
 
         game_file.close()
+        
 
+
+new_game = Game(1, 1, 1)
+ran_num = new_game.generate_random_num()
+inp = new_game.get_user_input()
+new_game.validate_and_play(inp, ran_num)
